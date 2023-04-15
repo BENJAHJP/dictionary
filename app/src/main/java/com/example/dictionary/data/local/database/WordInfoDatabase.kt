8d5a01d3c14df411473e689914814cec1cb2a026
benjahjp <benjahjp@gmail.com>
2023-04-15 21:35:18 +0300

    feat: local database and converters

--- app/src/main/java/com/example/dictionary/data/local/database/WordInfoDatabase.kt
+++ app/src/main/java/com/example/dictionary/data/local/database/WordInfoDatabase.kt
@@ -2,6 +2,8 @@ package com.example.dictionary.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.dictionary.data.local.Converters
 import com.example.dictionary.data.local.WordInfoDao
 import com.example.dictionary.data.local.entity.WordInfoEntity
 
@@ -9,6 +11,7 @@ import com.example.dictionary.data.local.entity.WordInfoEntity
     entities = [WordInfoEntity::class],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class WordInfoDatabase: RoomDatabase() {
     abstract val dao: WordInfoDao
 }
