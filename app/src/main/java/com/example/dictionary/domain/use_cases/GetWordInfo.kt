4a6346559d76b06eaaf5ff9587cf5c82e5a6600e
benjahjp <benjahjp@gmail.com>
2023-04-15 21:54:40 +0300

    feat: use case

---
+++ app/src/main/java/com/example/dictionary/domain/use_cases/GetWordInfo.kt
@@ -0,0 +1,18 @@
+package com.example.dictionary.domain.use_cases
+
+import com.example.dictionary.common.Resource
+import com.example.dictionary.domain.model.WordInfo
+import com.example.dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetWordInfo(
+    private val repository: WordInfoRepository
+) {
+    operator fun invoke(word: String): Flow<Resource<List<WordInfo>>>{
+        if (word.isBlank()){
+            return flow {  }
+        }
+        return repository.getWordInfo(word)
+    }
+}
