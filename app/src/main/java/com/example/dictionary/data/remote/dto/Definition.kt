8b2b7a6c810089dca890ce355f4fe3e4f79425b1
benjahjp <benjahjp@gmail.com>
2023-04-15 20:18:01 +0300

    feat domain model

--- app/src/main/java/com/example/dictionary/data/remote/dto/Definition.kt
+++ app/src/main/java/com/example/dictionary/data/remote/dto/Definition.kt
@@ -3,6 +3,15 @@ package com.example.dictionary.data.remote.dto
 data class Definition(
     val antonyms: List<String>,
     val definition: String,
-    val example: String,
+    val example: String?,
     val synonyms: List<String>
-)
+){
+    fun toDefinition(): Definition{
+        return Definition(
+            antonyms = antonyms,
+            definition = definition,
+            example = example,
+            synonyms = synonyms
+        )
+    }
+}
