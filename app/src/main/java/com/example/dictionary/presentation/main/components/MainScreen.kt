5712954ea9df310c5edc9dd97c3b1bca348f4105
benjahjp <benjahjp@gmail.com>
2023-04-17 19:49:08 +0300

    feat: circular indicator

--- app/src/main/java/com/example/dictionary/presentation/main/components/MainScreen.kt
+++ app/src/main/java/com/example/dictionary/presentation/main/components/MainScreen.kt
@@ -2,7 +2,6 @@ package com.example.dictionary.presentation.main.components
 
 import android.annotation.SuppressLint
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
