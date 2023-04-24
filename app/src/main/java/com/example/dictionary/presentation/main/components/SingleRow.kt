6c93c0c2812139851a4f8b9c68958e3ae36ea4e3
benjahjp <benjahjp@gmail.com>
2023-04-24 18:10:43 +0300

    fix: expandable card

--- app/src/main/java/com/example/dictionary/presentation/main/components/SingleRow.kt
+++ app/src/main/java/com/example/dictionary/presentation/main/components/SingleRow.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,31 +25,35 @@ fun SingleRow(
     wordInfo: WordInfo,
 ) {
     OutlinedCard(
-        modifier = Modifier.fillMaxWidth()
-            .padding(10.dp)
-            .clickable { onExpand(true) }
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .clickable { onExpand(!isExpanded) }
         ,
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(16.dp)
     ) {
-        wordInfo.word?.let {
-            Text(
-                text = it,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-        if (isExpanded){
-            wordInfo.phonetic?.let { Text(text = it, fontWeight = FontWeight.Light) }
-            Spacer(modifier = Modifier.height(16.dp))
-            wordInfo.meanings?.forEach { meaning ->
-                Text(text = meaning.partOfSpeech, fontWeight = FontWeight.Bold)
-                meaning.definitions.forEachIndexed { i, definition ->
-                    Text(text = "${i + 1}. ${definition.definition}")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    definition.example?.let { example ->
-                        Text(text = "Example: $example")
+        Column(modifier = Modifier.padding(16.dp)) {
+            wordInfo.word?.let {
+                Text(
+                    text = it,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            if (isExpanded){
+                wordInfo.phonetic?.let { Text(text = it, fontWeight = FontWeight.Light) }
+                Spacer(modifier = Modifier.height(16.dp))
+                wordInfo.meanings?.forEach { meaning ->
+                    Text(text = meaning.partOfSpeech, fontWeight = FontWeight.Bold)
+                    meaning.definitions.forEachIndexed { i, definition ->
+                        Text(text = "${i + 1}. ${definition.definition}")
+                        Spacer(modifier = Modifier.height(8.dp))
+                        definition.example?.let { example ->
+                            Text(text = "Example: $example")
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
                     }
-                    Spacer(modifier = Modifier.height(8.dp))
                 }
             }
         }
